481c4d231f75e62a50565f8dfa74bb182cd9ac16
pankajrajputiet <95834257+pankajrajputiet@users.noreply.github.com>
2021-12-25 08:39:43 +0530

    Create bubble.js

---
+++ sort/bubble.js
@@ -0,0 +1,15 @@
+const number = [99,44,6,2,1,5,63,87,283,4,0];
+
+function bubbleSort(array){
+  for(let i =0;i<array.length;i++){
+    for(let j =0;j<array.length;j++){
+    if(array[j] > array[j+1]){
+      let temp = array[j+1];
+      array[j+1] = array[j];
+      array[j] = temp;
+    }
+    }
+  }
+  }
+  bubbleSort(number);
+  console.log(number);
